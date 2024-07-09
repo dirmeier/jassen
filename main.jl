fe2dec6512f695581c3573e03d9b2f3e709fc806
Simon Dirmeier <simd23@protonmail.com>
2024-07-09 19:10:00 +0200

    Update

--- main.jl
+++ main.jl
@@ -1,17 +1,14 @@
 #!/usr/bin/julia
 
-include("src/model/player/greedy_player.jl")
-include("src/model/game.jl")
-include("src/controller.jl")
+import Jassen: GreedyPlayer, Game
 
 (
     function ()
         players = [
             GreedyPlayer(1, 1), GreedyPlayer(2, 2), GreedyPlayer(3, 1), GreedyPlayer(4, 2)
         ]
-        game = Game(players, 12345)
-        controller = Controller(game)
-        play_round(controller)
+        game = Game(players, 12345)        
+        play_round(game)
         return println("done")
     end
 )()

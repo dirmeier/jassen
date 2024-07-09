e4b9f1ab63c2b3b79d2b1b85d1aeb72df133df39
Simon Dirmeier <simd23@protonmail.com>
2024-07-09 19:32:23 +0200

    Update

--- main.jl
+++ main.jl
@@ -8,7 +8,8 @@ include("src/model/game.jl")
         players = [
             GreedyPlayer(1, 1), GreedyPlayer(2, 2), GreedyPlayer(3, 1), GreedyPlayer(4, 2)
         ]
-        game = Game(players, 12345)        
+        game = Game(players, 12345)
+
         play_round(game)
         return println("done")
     end

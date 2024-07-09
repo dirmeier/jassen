9949317db12d94794ccdd0c57f28163fc3f8953e
Simon Dirmeier <dirmeier@users.noreply.github.com>
2024-07-09 19:03:06 +0200

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/jassen.jl
+++ src/jassen.jl
@@ -1,17 +1,17 @@
 module Jassen
 
-    import Random: shuffle
-    import StatsBase: countmap
-    import StableRNGs: StableRNG
-    
-    include(joinpath("model", "card.jl"))
-    include(joinpath("model", "trick.jl"))
-    include(joinpath("model", "round.jl"))
-    
-    include(joinpath("model", "player", "player.jl"))
-    include(joinpath("model", "player", "ai_player.jl"))
-    include(joinpath("model", "player", "greedy_player.jl"))
-    include(joinpath("model", "player", "human_player.jl"))    
-    
-    include(joinpath("model", "game.jl"))
+import Random: shuffle
+import StatsBase: countmap
+import StableRNGs: StableRNG
+
+include(joinpath("model", "card.jl"))
+include(joinpath("model", "trick.jl"))
+include(joinpath("model", "round.jl"))
+
+include(joinpath("model", "player", "player.jl"))
+include(joinpath("model", "player", "ai_player.jl"))
+include(joinpath("model", "player", "greedy_player.jl"))
+include(joinpath("model", "player", "human_player.jl"))
+
+include(joinpath("model", "game.jl"))
 end

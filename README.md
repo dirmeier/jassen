a05562aa8b4a76dccfa22ab68a92d4209bf64d5c
Simon Dirmeier <dirmeier@users.noreply.github.com>
2024-07-10 11:58:58 +0200

    Implement custom players (#2)

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
-# jassen
+# Jassen
 
-[![Build Status](https://github.com/dirmeier/jassen.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/dirmeier/jassen.jl/actions/workflows/CI.yml?query=branch%3Amain)
+[![ci](https://github.com/dirmeier/jassen/actions/workflows/ci.yaml/badge.svg?branch=main)](https://github.com/dirmeier/jassen/actions/workflows/ci.yaml?query=branch%3Amain)
+[![aqua](https://raw.githubusercontent.com/JuliaTesting/Aqua.jl/master/badge.svg)](https://github.com/JuliaTesting/Aqua.jl)
+
+> Learning Jassen with reinforcement learning
+
+## About
+
+`Jassen` is a Julia implementation of the popular Swiss card game. It implements a simple command-line interface and offers greedy and RL trained agents as opponents.
+
+## Usage
+
+To play a game of Jassen, call
+
+```bash
+cd jasssen
+julia --project=. main.jl
+```
+
+This creates a game with 4 greedy bots.
+
+To play yourself against the RL agent (or multiple RL agents), you can specify player types using
+
+```bash
+julia --project=. main.jl --player1 human --player2 ai
+```
+
+## Installation
+
+Install [Julia](https://julialang.org/downloads/) and then download the repository via:
+
+```bash
+git clone https://github.com/dirmeier/jassen.git
+```
+
+Then install all dependencies using
+
+```bash
+cd jassen
+julia --project=. -e "import Pkg; Pkg.instantiate();"
+```
+
+
+## Author
+
+Simon Dirmeier <a href="mailto:sfyrbnd @ pm me">sfyrbnd @ pm me</a>
--- main.jl
+++ main.jl
@@ -1,17 +1,47 @@
 #!/usr/bin/julia
 
+using ArgParse
+
 include("src/model/player/greedy_player.jl")
 include("src/model/player/human_player.jl")
+include("src/model/player/ai_player.jl")
 include("src/model/game.jl")
 
 (
-    function ()
+    function (args)
+        settings = ArgParseSettings()
+        player_types = ["human", "greedy", "agent"]
+        @add_arg_table settings begin
+            "--player1"
+                help = "what kind of player is player 1; must be one of " *  join(player_types, ", ")
+                default = "greedy"
+                range_tester = (x -> x in player_types)
+            "--player2"
+                help = "what kind of player is player 2; must be one of " *  join(player_types, ", ")
+                default = "greedy"
+                range_tester = (x -> x in player_types)
+            "--player3"
+                help = "what kind of player is player 3; must be one of " *  join(player_types, ", ")
+                default = "greedy"
+                range_tester = (x -> x in player_types)
+            "--player4"
+                help = "what kind of player is player 4; must be one of " *  join(player_types, ", ")
+                default = "greedy"
+                range_tester = (x -> x in player_types)
+        end
+        args = parse_args(args, settings)        
+
+        function dispatch(tag)
+            return Dict("human" => HumanPlayer, "greedy" => GreedyPlayer, "ai" => AIPlayer)[tag]
+         end
+
         players = [
-            HumanPlayer(1, 1), GreedyPlayer(2, 2), GreedyPlayer(3, 1), GreedyPlayer(4, 2)
+            dispatch(args[string("player", i)])(i, i % 4)
+            for i in range(1, 4)
         ]
         game = Game(players, 12345)
 
         play_round(game)
         return println("done")
     end
-)()
+)(ARGS)
